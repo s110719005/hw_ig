06d4e42feb2735a2425a726f8abb0ddf72d88204
Alice Wang <alicewang@Alicede-MacBook-Pro-2.local>
2020-04-12 21:56:51 +0800

    message header

--- src/screens/MessageScreen.js
+++ src/screens/MessageScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { ScrollView, Linking,View,Image,TouchableOpacity,StyleSheet } from 'react-native';
 import { Button, Card, Text, PricingCard, Tile } from 'react-native-elements';
 
+import albumData from "../json/albums.json";
 
 const MessageScreen = ({navigation}) => {
   
@@ -9,11 +10,28 @@ const MessageScreen = ({navigation}) => {
   return (
     <View>
       <View style={styles.headerStyle}>
-      <TouchableOpacity onPress={() => navigation.goBack('Home')}>
-              <Image style={{ width: 24, height: 24 }}
-                source={require('../../assets/send.png')}
-              />
-        </TouchableOpacity>
+        <View style={styles.headerleftStyle}>
+          <TouchableOpacity onPress={() => navigation.goBack('Home')}>
+            <Image style={{ width: 20, height: 20 ,marginLeft:10}}
+              source={require('../../assets/back.png')}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.headermiddleStyle}>
+          <Text style={{fontWeight:"600"}}>{albumData.accountname}</Text>
+        </View>
+
+        <View style={styles.headerrightStyle}>
+          <Image style={{ width: 24, height: 24 ,marginLeft:0}}
+              source={require('../../assets/film.png')}
+          />
+          <Image style={{ width: 24, height: 24 ,marginLeft:20}}
+            source={require('../../assets/edit.png')}
+          />
+        </View>
+        
+        
       </View>
       
       <ScrollView>
@@ -30,15 +48,29 @@ const MessageScreen = ({navigation}) => {
 const styles = StyleSheet.create({
   headerStyle: {
     backgroundColor: "#F8F8F8",
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
-    height: 90,
+    flexDirection:"row",
+    height: 80,
     paddingTop: 30,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
+    borderBottomWidth:0.5,
+    borderBottomColor:"#A3A399",
     // Android Only
     elevation: 2
+  },
+  headerleftStyle:{
+    width:"30%",
+    
+  },
+  headermiddleStyle:{
+    width:"40%",
+    alignItems: "center",
+  },
+  headerrightStyle:{
+    width:"30%",
+    flexDirection:"row",
+    justifyContent: "flex-end",
+    paddingRight:10,
   },
 });
 
